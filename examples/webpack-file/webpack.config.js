8ee71943f316dfea84be21a0097a2d81181e7825
Denis Domanskii <denis.domanskii@jetbrains.com>
2020-06-05 15:31:10 +0300

    add mini-css-extract-plugin

--- examples/webpack-file/webpack.config.js
+++ examples/webpack-file/webpack.config.js
@@ -1,13 +1,29 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = () => {
+  const extractor = new MiniCssExtractPlugin()
+
+  const plugins = [extractor]
+
+  const webpackConfig = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-    ],
-  },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+
+    plugins,
+  }
+
+  return webpackConfig
 }
